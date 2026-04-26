2fc33ba0eb0876655e53bf75820980ccee319e88
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-04-26 15:55:57 +0800

    Add helpers to compute hash values for parameter tables
    tests and checks passed.

---
+++ tests/testthat/test-digest.R
@@ -0,0 +1,60 @@
+library(testthat)
+suppressMessages(library(lavaan))
+
+test_that("digest", {
+
+mod1 <-
+"
+fx =~ x1 + x2 + x3
+fm =~ m1 + m2 + m3
+fy =~ y1 + y2 + y3
+fm ~ fx
+fy ~ fm + fx
+"
+fit1 <- sem(
+          model = mod1,
+          data = data_test_3_factor_3_item
+        )
+pt1 <- parameterTable(fit1)
+
+mod2 <-
+"
+fm =~ m1 + m2 + m3
+fx =~ x1 + x2 + x3
+fy =~ y1 + y2 + y3
+fy ~ fm + fx
+fm ~ fx
+"
+fit2 <- sem(
+          model = mod2,
+          data = data_test_3_factor_3_item
+        )
+pt2 <- parameterTable(fit2)
+
+expect_false(identical(pt1, pt2))
+
+pt1a <- sort_ptable(pt1)
+pt2a <- sort_ptable(pt2)
+
+cols0 <- c("lhs", "op", "rhs", "block", "group", "ustart")
+expect_equal(pt1a[, cols0],
+             pt2a[, cols0],
+             ignore_attr = TRUE)
+
+pt1a_digest <- digest_ptable(pt1a)
+pt2a_digest <- digest_ptable(pt2a)
+
+pt1b <- add_digest(pt1)
+pt2b <- add_digest(pt2)
+
+expect_false(identical(pt1b, pt2b))
+expect_identical(attr(pt1b, "digest"),
+                 attr(pt2b, "digest"))
+
+expect_identical(attr(pt1b, "digest"),
+                 get_digest(pt1b))
+
+expect_identical(attr(pt1b, "digest"),
+                 get_digest(pt1))
+
+})
