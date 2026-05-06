102994d759f9de40779b5751a9a0f2bc3ea149d2
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-05-06 22:45:04 +0800

    WIP: Draft gen_eq_models()
    Not yet ready.

---
+++ tests/testthat/test-all_in_one.R
@@ -0,0 +1,182 @@
+skip("WIP")
+
+library(testthat)
+suppressMessages(library(lavaan))
+
+test_that("+/- df: fit_models", {
+
+mod <-
+"
+fx =~ x1 + x2 + x3
+fm =~ m1 + m2 + m3
+fy =~ y1 + y2 + y3
+fm ~ fx
+fy ~ fm + fx
+"
+fit <- do.call(
+          sem,
+          list(
+            model = mod,
+            data = data_test_3_factor_3_item
+        ))
+pt <- parameterTable(fit)
+
+path_all <- function(
+  object
+) {
+  for (pt in object) {
+    i1 <- pt$lhs %in% c("fx", "fm", "fy")
+    i2 <- pt$rhs %in% c("fx", "fm", "fy")
+    i3 <- pt$lhs != pt$rhs
+    i <- i1 & i2 & i3
+    print(pt[i, c("lhs", "op", "rhs", "start", "est", "free")])
+    flush.console()
+  }
+}
+
+path_all_list <- function(
+  object
+) {
+  for (i in seq_along(object)) {
+    cat("i: ", i, "\n")
+    path_all(object[[i]])
+  }
+}
+
+# ==== eq_models ====
+
+gen_eq_models <- function(
+  sem_out,
+  parallel = FALSE,
+  progress = TRUE
+) {
+  # Add other arguments later
+
+  pt <- lavaan::parameterTable(sem_out)
+  models <- list(original = pt)
+  class(models) <- c("eq_partables", "partables", "list")
+  attr(models$original, "fit") <- sem_out
+  out <- list()
+  out_drop_tried <- list()
+  out_add_tried <- list()
+  k_old <- -1
+  k_new <- 0
+  while (k_old < k_new) {
+    k_old <- length(out)
+    if (length(out) == 0) {
+      out_i <- models
+      sem_out0 <- sem_out
+    } else {
+      out_i <- out
+      sem_out0 <- NULL
+    }
+    out_i <- setdiff_eq_partables(
+              out_i,
+              out_add_tried
+            )
+    # path_all(tmp)
+    out_drop_i <- lapply(
+      out_i,
+      drop_k,
+      sem_out = sem_out0,
+      fit_models = TRUE,
+      parallel = parallel,
+    )
+    if (length(out_add_tried) == 0) {
+      out_add_tried <- out_i
+    } else {
+      out_add_tried <- c(out_add_tried, out_i)
+    }
+    # path_all_list(out_drop_i)
+    out_drop_i <- combine_ptables(
+              out_drop_i
+            )
+    out_drop_i <- setdiff_eq_partables(
+              out_drop_i,
+              out_drop_tried
+            )
+    # path_all(out_drop_i)
+    out_add_i <- lapply(
+      out_drop_i,
+      add_k,
+      sem_out = sem_out,
+      fit_models = TRUE,
+      parallel = parallel
+    )
+    if (length(out_drop_tried) == 0) {
+      out_drop_tried <- out_drop_i
+    } else {
+      out_drop_tried <- c(out_drop_tried, out_drop_i)
+    }
+    # path_all_list(out_add_i)
+    out_add_i <- combine_ptables(
+              out_add_i
+            )
+    # path_all(out_add_i)
+    if (length(out) > 0) {
+      out <- c(out, out_add_i)
+    } else {
+      out <- out_add_i
+    }
+    # path_all(out)
+    k_new <- length(out)
+    if (progress) {
+      k_diff <- k_new - k_old
+      cat(k_diff, "new model(s); ",
+          k_new, "model(s) found\n")
+    }
+  }
+  out
+}
+
+out <- gen_eq_models(
+  sem_out = fit,
+  parallel = FALSE
+)
+out
+
+# TO PROCESS
+
+# ==== Test: drop_k ====
+
+fit_1_more1_with_fit <- drop_k(fit,
+              fit_models = TRUE,
+              parallel = FALSE)
+expect_s4_class(attr(fit_1_more1_with_fit[[1]], "fit"),
+                "lavaan")
+expect_equal(
+  lavInspect(eq_fits(fit_1_more1_with_fit)[[1]], "sampstat")$cov,
+  lavInspect(fit, "sampstat")$cov
+)
+
+# ==== Test: add_k ====
+
+fit_1_less_with_fit <- add_k(
+                fit_1_more1_with_fit[[1]],
+                sem_out = fit,
+                add_name = TRUE,
+                fit_models = TRUE,
+                parallel = FALSE
+              )
+expect_s4_class(attr(fit_1_less_with_fit[[1]], "fit"),
+                "lavaan")
+expect_equal(
+  lavInspect(eq_fits(fit_1_less_with_fit)[[1]], "sampstat")$cov,
+  lavInspect(fit, "sampstat")$cov
+)
+
+# ==== Test: drop_k: Using add_k output ====
+
+fit_1_more1_from_less1_with_fit <- drop_k(
+              fit_1_less_with_fit[[1]],
+              sem_out = fit,
+              fit_models = TRUE,
+              parallel = FALSE)
+expect_s4_class(attr(fit_1_more1_from_less1_with_fit[[1]], "fit"),
+                "lavaan")
+expect_equal(
+  lavInspect(eq_fits(fit_1_more1_from_less1_with_fit)[[1]], "sampstat")$cov,
+  lavInspect(fit, "sampstat")$cov
+)
+
+})
