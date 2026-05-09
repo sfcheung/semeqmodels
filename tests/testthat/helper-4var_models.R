62e548094be600fb3bf189351219ec7b504cb0e2
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-05-09 18:56:32 +0800

    Add do.fit = FALSE
    Also rename the output to pt_list_4_lav

--- tests/testthat/helper-4var_models.R
+++ tests/testthat/helper-4var_models.R
@@ -1319,14 +1319,17 @@ mod131 <- c(
   "fy ~ fm2"
 )
 
-pt_list <- list()
-
-for (i in 1:131) {
-  fit <- sem(
-    model = get(paste0("mod", i)),
-    data  = data_test_4_factor_3_item
+system.time(
+  pt_list_4_lav <- lapply(
+    seq_len(131),
+    \(x) {
+      fit <- sem(
+        model = get(paste0("mod", x)),
+        data  = data_test_4_factor_3_item,
+        do.fit = FALSE
+      )
+      parameterTable(fit)
+    }
   )
-  pt_list[[i]] <- parameterTable(fit)
-}
+)
 
-pt_list
