9f0c126614ba2f26436234f00e94ed962c02393d
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-09 08:13:35 +0800

    Create the helper files

---
+++ tests/testthat/helper-3obvar_models_dag.R
@@ -0,0 +1,4 @@
+# Use a DAG package to generate
+# a set of equivalent models from
+# a simple mediation model.
+# This set will be used in tests.
---
+++ tests/testthat/helper-4obvar_models_dag.R
@@ -0,0 +1,5 @@
+# Use a DAG package to generate
+# a set of equivalent models from
+# a serial mediation model or just
+# a model with four variables.
+# This set will be used in tests.
