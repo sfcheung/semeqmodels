a33571d81b7bb4481fef23951f51fc9ae6d9f04d
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-11 07:50:47 +0800

    Add save_history ot eq_df_models()
    tests and checks passed.
    History is now by default *not* saved.

--- tests/testthat/test-eq_df_models_history.R
+++ tests/testthat/test-eq_df_models_history.R
@@ -26,7 +26,8 @@ set.seed(1)
 out <- eq_df_models(
   sem_out = fit,
   parallel = FALSE,
-  progress = !is_testing()
+  progress = !is_testing(),
+  save_history = TRUE
 )
 out
 
@@ -34,7 +35,8 @@ out1 <- eq_models(
           out,
           original_model = fit,
           parallel = FALSE,
-          progress = !is_testing()
+          progress = !is_testing(),
+          eq_df_models_args = list(save_history = TRUE)
         )
 out1
 
