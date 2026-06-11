a4330fe263f746d64ac8ce92abdb5701f4621913
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-11 22:50:09 +0800

    Fix partable_fixedx for RAM
    tests and checks passed.

--- R/one_df_helpers.R
+++ R/one_df_helpers.R
@@ -696,10 +696,15 @@ partable_fixedx <- function(
       model = partable,
       do.fit = FALSE
     )
-    tmp <- lavaan::lavInspect(fit0,
-            "free",
-            drop.list.single.group = FALSE
-          )[[1]]$psi
+    tmp0 <- lavaan::lavInspect(fit0,
+                  "free",
+                  drop.list.single.group = FALSE
+                )[[1]]
+    tmp <- switch(
+      fit0@Options$representation,
+      LISREL = tmp0$psi,
+      RAM = tmp0$A
+    )
     fixed.x <- any(diag(tmp)[xnames] == 0)
   } else {
     # No observed variables
