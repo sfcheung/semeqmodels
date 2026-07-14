50b17a1006bc953dc9b2edfbecd7abb608a92c53
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-07-15 07:57:48 +0800

    WIP: lav_4 drafted but not ready

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -13,5 +13,6 @@ setwd(base_dir)
 setwd(paste0(base_dir, "/vignettes/articles/"))
 
 knitr::knit("demo_lav_3.Rmd.original", output = "demo_lav_3.Rmd")
+knitr::knit("demo_lav_4.Rmd.original", output = "demo_lav_4.Rmd")
 
 setwd(base_dir)
