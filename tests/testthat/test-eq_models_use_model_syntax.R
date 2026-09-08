c532e09a81befcbe1558c49624d0971ed604d6c9
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-09-08 21:23:25 +0800

    Enable the test
    test passed

--- tests/testthat/test-eq_models_use_model_syntax.R
+++ tests/testthat/test-eq_models_use_model_syntax.R
@@ -1,4 +1,4 @@
-skip("WIP")
+skip_on_cran()
 
 library(testthat)
 suppressMessages(library(lavaan))
@@ -7,31 +7,14 @@ test_that("Set original model", {
 
 mod <-
 "
-fx =~ x1 + x2 + x3
-fm =~ m1 + m2 + m3
-fy =~ y1 + y2 + y3
 fm ~ fx
 fy ~ fm + fx
 "
 out1 <- eq_models(
   original_model = mod,
-  progress = !is_testing()
+  progress = !is_testing(),
+  parallel = FALSE
 )
-
-fit <- sem(
-  mod,
-  do.fit = FALSE
-)
-tmp <- dummy_data(parameterTable(fit))
-fit <- sem(
-  mod,
-  data = tmp
-)
-out2 <- eq_models(
-  original_model = fit,
-  progress = !is_testing()
-)
-
 layout_i <- matrix(c(  NA, "fm",  NA,
                      "fx",   NA, "fy"),
                    ncol = 3,
@@ -43,28 +26,15 @@ p1 <- partables_plots(
   label.cex = 1.5,
   sizeLat = 15,
   edge.width = 5,
-  asize = 5,
-  structural = TRUE
+  asize = 5
+)
+expect_s3_class(
+  attr(p1, "original_model")[[1]],
+  "qgraph"
 )
 if (!is_testing()) {
 plot(
   p1,
-  ncol = 4,
-  nrow = 4
-)
-}
-p2 <- partables_plots(
-  out2,
-  layout = layout_i,
-  label.cex = 1.5,
-  sizeLat = 15,
-  edge.width = 5,
-  asize = 5,
-  structural = TRUE
-)
-if (!is_testing()) {
-plot(
-  p2,
   ncol = 4,
   nrow = 4
 )
